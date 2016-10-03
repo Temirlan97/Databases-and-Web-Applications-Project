a5d88a022ba9986f88f158344d4be7425880343c
ypan <ypan@localhost.localdomain>
2016-10-03 21:57:21 +0200

    All entities added

--- schema/schema.sql
+++ schema/schema.sql
@@ -55,25 +55,55 @@ CREATE TABLE study(
 	PRIMARY KEY (st_id)
 );
 
+CREATE TABLE travel(
+	tr_id CHAR(20) NOT NULL REFERENCES event(e_id),
+	budget int NOT NULL,
+	PRIMARY KEY (tr_id) 
+);
+
+CREATE TABLE sport(
+	sp_id CHAR(20) NOT NULL REFERENCES event(e_id),
+	game_name CHAR(20) NOT NULL,
+	PRIMARY KEY(sp_id)
+);
+
+CREATE TABLE board_game(
+	bo_id CHAR(20) NOT NULL REFERENCES event(e_id),
+	game_name CHAR(20) NOT NULL,
+	PRIMARY KEY(bo_id)
+);
+
+CREATE TABLE movie(
+	mo_id CHAR(20) NOT NULL REFERENCES event(e_id),
+	movie_name CHAR(20) NOT NULL,
+	PRIMARY KEY(mo_id)
+);
+
+CREATE TABLE party(
+	pa_id CHAR(20) NOT NULL REFERENCES reason(e_id),
+	reason CHAR(20) NOT NULL,
+	PRIMARY KEY(pa_id)
+);
+
 CREATE TABLE create(
-	u_id CHAR(20) NOT NULL,
-	e_id CHAR(20) NOT NULL,
-	FOREIGN KEY u_id NOT NULL REFERENCES users(u_id),
-	FOREIGN KEY e_id NOT NULL REFERENCES event(e_id),
-	PRIMARY KEY (e_id)
+	c_u_id CHAR(20) NOT NULL,
+	c_e_id CHAR(20) NOT NULL,
+	PRIMARY KEY (c_e_id),
+	FOREIGN KEY c_u_id NOT NULL REFERENCES users(u_id) ON DELETE CASCADE,
+	FOREIGN KEY c_e_id NOT NULL REFERENCES event(e_id) ON DELETE CASCADE
 );
 
 CREATE TABLE join(
 	u_id CHAR(20) NOT NULL,
 	e_id CHAR(20) NOT NULL,
-	FOREIGN KEY u_id NOT NULL REFERENCES users(u_id),
-	FOREIGN KEY e_id NOT NULL REFERENCES event(e_id),
-	PRIMARY KEY (e_id, u_id)	
+	PRIMARY KEY (e_id, u_id),	
+	FOREIGN KEY u_id NOT NULL REFERENCES users(u_id) ON DELETE CASCADE,
+	FOREIGN KEY e_id NOT NULL REFERENCES event(e_id) ON DELETE CASCADE
 );
 
 CREATE TABLE take_place_in(
 	l_id CHAR(20) NOT NULL,
 	e_id CHAR(20) NOT NULL,
-	FOREIGN KEY l_id NOT NULL REFERENCES users(l_id),
-	FOREIGN KEY e_id NOT NULL REFERENCES event(e_id)
+	FOREIGN KEY l_id NOT NULL REFERENCES users(l_id) ON DELETE CASCADE,
+	FOREIGN KEY e_id NOT NULL REFERENCES event(e_id) ON DELETE CASCADE
 );
