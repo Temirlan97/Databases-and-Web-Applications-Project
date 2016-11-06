0d0c12b109998e9ac0b3189008b28e6879372855
ypan <ypan@localhost.localdomain>
2016-11-06 22:58:27 +0100

    Forms done

--- html/src/input.php
+++ html/src/input.php
@@ -5,15 +5,40 @@
   <form>
     Please select the type of your event:
     <select name="event">
+    <option>----</option>
     <option value="party">Party</option>
     <option value="movie">Movie</option>
     <option value="boardgame">Boardgame</option>
     <option value="travel">Travel</option>
-  </select>
-  <br>
+    </select>
+    <br>
     Please enter event name:
-    <input type="text" name="boardgame"><br><br>
+    <input type="text" name="boardgame">
+    <br><br>
     <input type="submit" value="Submit">
+    <br><br><br><br>
+
+    Username:<input type="text" name="username">
+    <br>
+    Password:<input type="text" name="password">
+    <br>
+    Gender:
+    <select name="gender">
+      <option>----</option>
+      <option value="male">Male</option>
+      <option value="female">Female</option>
+    </select>
+    <br><br>
+    <input type="submit" value="Sign up">
+    <br><br><br><br>
+
+    Username:<input type="text" name="username">
+    <br>
+    Eventname:<input type="text" name="eventname">
+    <br><br>
+    <input type="submit" value="Join">
+    <br><br><br><br>
+
   </form>
 </div>
 
